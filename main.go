dfaff5f1c98ed6207f721aec386807ba7c9c8f06
Boluwatife Ayodele <waplurd1@gmail.com>
2021-06-13 15:19:34 +0100

    created  a route to create link

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 )
 
 func main(){
-		router := mux.NewRouter()
-		
-
+{	router := mux.NewRouter()
+	router.HandleFunc("/api/newlink", controllers.CreateLink).Methods("POST", "OPTIONS")
+	router.HandleFunc("/", controllers.Welcome).Methods("POST", "OPTIONS")
 	fmt.Println("Starting server on the port 5000")
 	fmt.Println(dbconnection.Createconnection())
 
