1da66ec982f3e1e1ce032c648839643a763aa12e
Boluwatife Ayodele <waplurd1@gmail.com>
2021-06-20 15:22:53 +0100

    Added an endpoint fot updating db records

--- controllers/links.controller.go
+++ controllers/links.controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	dbconnection "linkscape/db_connection"
@@ -8,31 +9,31 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"database/sql"
+
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 type Createresponse struct {
 	ID      int64  `json:"id,omitempty"`
-	Title      string  `json:"title,omitempty"`
-	Url 	string `json:"url,omitempty"`
-    Message string `json:"message,omitempty"`
+	Title   string `json:"title,omitempty"`
+	Url     string `json:"url,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
 type response struct {
-    Message string `json:"message,omitempty"`
+	Status bool `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
-func Welcome(w http.ResponseWriter, r *http.Request){
+func Welcome(w http.ResponseWriter, r *http.Request) {
 	res := response{
-     Message: "User created successfully",
-    }
+		Message: "User created successfully",
+	}
 
-    // send the response
-    json.NewEncoder(w).Encode(res)
+	// send the response
+	json.NewEncoder(w).Encode(res)
 }
-
 
 func CreateLink(w http.ResponseWriter, r *http.Request) {
 	//setting up headers
@@ -46,77 +47,76 @@ func CreateLink(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&link)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-        return
+		return
 	}
-	
+
 	// create the postgres db connection
-	 
+
 	db := dbconnection.Createconnection()
 	// close the db connection
 	defer db.Close()
 	sqlStatement := `INSERT INTO links (title, link, categoryid, userid) VALUES ($1, $2, $3, $4) RETURNING linkId, link,title`
-
-    // the inserted id will store in this id
+	// the inserted id will store in this id
 	var linkId int64
-	var linkTitle,  linkUrl string
+	var linkTitle, linkUrl string
 
-    // execute the sql statement
-    // Scan function will save the insert id in the id
-    query:= db.QueryRow(sqlStatement, link.Title, link.Link, link.CategoryId, link.UserId).Scan(&linkId, &linkUrl,&linkTitle)
+	// execute the sql statement
+	// Scan function will save the insert id in the id
+	query := db.QueryRow(sqlStatement, link.Title, link.Link, link.CategoryId, link.UserId).Scan(&linkId, &linkUrl, &linkTitle)
 
-    if query != nil {
-        log.Fatalf("Unable to execute the query. %v", query)
-    }
+	if query != nil {
+		log.Fatalf("Unable to execute the query. %v", query)
+	}
 
 	fmt.Printf("Inserted a single record %v", linkId)
-	
+
 	res := Createresponse{
 		ID:      linkId,
-		Title : linkTitle,
-		Url: linkUrl,
-        Message: "Link added successfully",
-    }
-// sending back json
+		Title:   linkTitle,
+		Url:     linkUrl,
+		Message: "Link added successfully",
+	}
+	// sending back json
 	json.NewEncoder(w).Encode(res)
 }
 
 //get link by id
-func GetLink(w http.ResponseWriter, r *http.Request){
+func GetLink(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")	
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET")
 
 	// Grabbing the id from the url
 	params := mux.Vars(r)
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		log.Fatalf("Unable to convert string into int %v",err)
+		log.Fatalf("Unable to convert string into int %v", err)
 	}
 	// create connection
 	db := dbconnection.Createconnection()
 	// close the db connection
-	defer db.Close()	
+	defer db.Close()
 	var link Models.Link
 	// sql statement
 	sqlStatement := `SELECT * FROM links WHERE linkid = $1`
 	// Execute sql statement
-	row := db.QueryRow(sqlStatement,id)
-	errFromStruct := row.Scan(&link.ID,&link.Title,&link.Link,&link.CategoryId,&link.UserId)
+	row := db.QueryRow(sqlStatement, id)
+	errFromStruct := row.Scan(&link.ID, &link.Title, &link.Link, &link.CategoryId, &link.UserId)
 	switch errFromStruct {
 	case sql.ErrNoRows:
 		res := response{
 			Message: "User created successfully",
-		   }
+		}
 		json.NewEncoder(w).Encode(res)
-    case nil:
-           json.NewEncoder(w).Encode(link)
-    default:
-        log.Fatalf("Unable to scan the row. %v", errFromStruct)
+	case nil:
+		json.NewEncoder(w).Encode(link)
+	default:
+		log.Fatalf("Unable to scan the row. %v", errFromStruct)
 	}
 
- }
+}
 
-func GetAllLink(w http.ResponseWriter,  r *http.Request){
+func GetAllLink(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET")
@@ -129,22 +129,69 @@ func GetAllLink(w http.ResponseWriter,  r *http.Request){
 	// sql statement
 	sqlStatement := `SELECT * FROM links`
 	// Execute sql statement
-	row,err := db.Query(sqlStatement)																				
-	if(err != nil){
+	row, err := db.Query(sqlStatement)
+	if err != nil {
 		res := response{
 			Message: "An error occured",
-		   }
+		}
 		json.NewEncoder(w).Encode(res)
 	}
-	for row.Next(){
-			errFromStruct := row.Scan(&link.ID,&link.Title,&link.Link,&link.CategoryId,&link.UserId)
-			if errFromStruct != nil {
-				res := response{
-					Message: "An error occured",
-				   }
-				json.NewEncoder(w).Encode(res)
+	for row.Next() {
+		errFromStruct := row.Scan(&link.ID, &link.Title, &link.Link, &link.CategoryId, &link.UserId)
+		if errFromStruct != nil {
+			res := response{
+				Message: "An error occured",
 			}
-			links = append(links,link)
+			json.NewEncoder(w).Encode(res)
 		}
-		json.NewEncoder(w).Encode(links)
+		links = append(links, link)
+	}
+	json.NewEncoder(w).Encode(links)
+}
+
+func UpdateLink(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("	Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	var link Models.Link
+
+	if err != nil {
+		log.Fatalf("Unable to convert string into int %v", err)
+	}
+	err = json.NewDecoder(r.Body).Decode(&link)
+
+    if err != nil {
+        log.Fatalf("Unable to decode the request body.  %v", err)
+    }
+	db := dbconnection.Createconnection()
+	// close the db connection
+	defer db.Close()
+	sqlStatement := `UPDATE links SET title=$2, link=$3, categoryid=$4, userId=$5 WHERE linkId=$1`
+	// execute the sql statement
+	result, err := db.Exec(sqlStatement,id, link.Title, link.Link, link.CategoryId, link.UserId)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query. %v", err)
+	}
+
+	// check how many rows affected
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+	if rowsAffected > 0 {
+		res := response{
+		Status : true,
+		Message: "Data updated succesfully!",
+	}
+	json.NewEncoder(w).Encode(res)
+}	
+	
+	fmt.Printf("Total rows/record affected %v", rowsAffected)
+
 }
--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 	"net/http"
 )
 
+	
 func main(){
 	router := mux.NewRouter()
 	router.HandleFunc("/api/newlink", controllers.CreateLink).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/links", controllers.GetAllLink).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/link/{id}", controllers.GetLink).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/link/{id}", controllers.UpdateLink).Methods("PUT", "OPTIONS")
+
 	router.HandleFunc("/", controllers.Welcome).Methods("POST", "OPTIONS")
 	fmt.Println("Starting server on the port 5000")
 	fmt.Println(dbconnection.Createconnection())
