7ec49c7a1e134cbf73525087123616e89fcb56d3
Boluwatife Ayodele <waplurd1@gmail.com>
2021-07-10 02:08:56 +0100

    done vendor folder

--- go.mod
+++ go.mod
@@ -7,3 +7,4 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.10.2
 )
+
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"linkscape/controllers"
 	"linkscape/db_connection"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 	
@@ -20,7 +22,8 @@ func main(){
 
 
 	router.HandleFunc("/", controllers.Welcome).Methods("POST", "OPTIONS")
+	port := os.Getenv("PORT")
 	fmt.Println("Starting server on the port 5000")
 	fmt.Println(dbconnection.Createconnection())
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(port, router))
 }
